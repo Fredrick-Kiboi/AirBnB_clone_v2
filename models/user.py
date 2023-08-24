7fbd348f29c21e399aa6a43e2a3b79f5cf66ce08
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-08-23 21:49:40 -0700

    Odo Peter Ebere - hope-this-checks

--- models/user.py
+++ models/user.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
+STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if storage_type == 'db':
+    if STORAGE == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
