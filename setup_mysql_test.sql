1586938633ecbf35c6d8386bc285cb01b75e42b7
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-08-26 16:04:45 -0700

    Odo Peter Ebere - fixes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
 -- Creating SQL hbnb_test_db database
--- Privileges for new user (hbnb_test)
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
